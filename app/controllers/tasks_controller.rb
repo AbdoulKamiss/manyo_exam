a8a4d9643b7084a9a1a59b872ea775f27508ec05
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-04-07 16:14:44 +0000

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,8 @@ class TasksController < ApplicationController
     def create
         @task = Task.new(task_params)
         if @task.save
-          redirect_to tasks_url, notice: "Task was succesfully created."
+          flash[:notice] = 'Task was successfully created.'
+          redirect_to tasks_url
         else
           render :new
         end
@@ -26,7 +27,8 @@ class TasksController < ApplicationController
 
     def update
         if @task.update(task_params)
-          redirect_to @task, notice: "Task was succesfully updated."
+          flash[:notice] = 'Task was successfully updated.'
+          redirect_to @task
         else
           render :edit
         end
@@ -34,7 +36,8 @@ class TasksController < ApplicationController
     
     def destroy
         @task.destroy
-        redirect_to tasks_url, notice: "Task was succesfully destroyed."
+        flash[:notice] = 'Task was successfully destroyed.'
+        redirect_to tasks_url
     end
 
     private
