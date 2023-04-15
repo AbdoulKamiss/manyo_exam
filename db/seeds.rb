e893dfed2ee3dae7851a7c4fcdb04f60775a95fe
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-04-15 13:10:45 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'date'
-require 'factory_bot_rails'
-
-date = Date.new(2025, 2, 18)
-
-(1..50).each do |n|
-  created_at = date -= 1
-  suffix = case n % 10
-    when 1 then "st"
-    when 2 then "nd"
-    when 3 then "rd"
-    else "th"
-  end
-  title = "#{n}#{suffix}_task"
-  task = FactoryBot.create(:task, title: title, created_at: created_at)
+50.times do |i|
+  Task.find_or_create_by(
+    title: "Task#{i+1}",
+    content: "Content#{i+1}", 
+  )
 end
