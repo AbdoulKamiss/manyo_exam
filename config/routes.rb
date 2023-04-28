2a0e1c95b27cd380e971edf8c68bdd4a8a4385eb
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-04-28 11:01:37 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  resources :users, except: :index
+  resources :sessions, only: %i(new create destroy)
+  namespace :admin do
+    resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  end
 end
