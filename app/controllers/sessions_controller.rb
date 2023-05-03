0844796622318adef4b55c4f41253ae7c68eab1f
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-05-03 07:54:10 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,35 @@
 class SessionsController < ApplicationController
-  skip_before_action :login_required, only: %i(new create)
+  include SessionsHelper
+  before_action :require_login, only: [:destroy]
 
   def new
+    redirect_to tasks_path if logged_in?
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in(user)
-      redirect_to tasks_path, notice: 'ログインしました'
+      flash[:notice] = 'ログインしました'
+      redirect_to tasks_path
     else
       flash.now[:alert] = 'メールアドレスまたはパスワードに誤りがあります'
-      render :new
+      render 'new'
     end
   end
 
   def destroy
-    session.delete(:user_id)
-    flash[:notice] = 'ログアウトしました'
-    redirect_to new_session_path
+    log_out
+    redirect_to new_session_path, notice: 'ログアウトしました'
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      store_location
+      flash[:alert] = "ログインしてください"
+      redirect_to new_session_path
+    end
   end
 end
